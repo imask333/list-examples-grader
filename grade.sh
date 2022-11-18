6841a826477abe52cc73a7cd4177401e561d9ec5
Hoang <114331111+imask333@users.noreply.github.com>
2022-11-17 23:23:45 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -2,6 +2,31 @@
 
 set -e
 
+rm -rf newDirectory
 rm -rf student-submission
+
 git clone $1 student-submission
 
+
+if [[ -e student-submission/ListExamples.java ]]
+then 
+    echo $?
+else
+    echo "failed"
+    exit 1
+fi
+
+javac -cp $CPATH *.java 2>compError.txt
+touch TestListExamples
+mv TestListExamples/student-submission/
+
+if [[$? -eq 0]]
+then 
+    echo "compile"
+else
+    echo "not commplie"
+    exit 1
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > codeError.txt
+
+javac -cp $CPATH *.java 2>compError.txt
